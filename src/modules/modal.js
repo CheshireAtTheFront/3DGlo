348704154faef133cdacd8122ee884a0118a6a9a
Diana Makarova <din_m-v@mail.ru>
2024-04-15 23:51:40 +0300

    процесс настройки анимации для модального окна в файле modal.js продолжается

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -2,29 +2,35 @@ const modal = () => {
   const modal = document.querySelector('.popup');
   const buttons = document.querySelectorAll('.popup-btn');
   const closeBtn = modal.querySelector('.popup-close');
-
-  console.log(closeBtn);
+  const modalContent = modal.querySelector('.popup-content');
   
   buttons.forEach(btn => {
     btn.addEventListener('click', () => {
       if (window.innerWidth > 768) {
         modal.style.display = 'block';
-        modal.style.transform = 'translateX(0)';
+        modalContent.style.opacity = '.5';
+        setTimeout(() => {
+          modalContent.style.opacity = '1';
+        }, 63);
       } else {
         modal.style.display = 'block';
       }
-      // modal.style.opacity = '1';
-      // modal.style.transition = 'opacity 0 1 1s linear 0s';
-      
     })
   })
 
   closeBtn.addEventListener('click', () => {
-    // let animation = modal.classList.add('opacity');
-    // modal.style.transform = "transition"
-    // modal.style.transition = 'opacity 0 1 1s linear 0s';
-    modal.style.display = 'none';
+    if (window.innerWidth > 768) {
+      modal.style.display = 'none';
+      modalContent.style.opacity = '0.5';
+      setTimeout(() => {
+        console.log('click');
+        modalContent.style.opacity = '0';
+      }, 1000);
+    } else {
+      modal.style.display = 'block';
+    }
   })
+
 }
 
 export default modal
