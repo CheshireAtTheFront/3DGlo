f28374a441396057f5c8f5b4dc6aa11a95ffbed2
Diana Makarova <din_m-v@mail.ru>
2024-05-21 00:03:25 +0300

    исправленно событие для тега LI в меню

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -2,22 +2,16 @@ const menu = () => {
   const menu = document.querySelector("menu");
   const body = document.querySelector("body");
 
-  const toggleMenu = () => {
-    body.addEventListener("click", (e) => {
-      let listMenu = e.target;
-      if (
-        listMenu.closest(".menu") ||
-        listMenu.classList.contains("close-btn")
-      ) {
-        menu.classList.toggle("active-menu");
-      } else if (!listMenu.matches("menu")) {
-        menu.classList.remove("active-menu");
-      } else {
-        menu.classList.add("active-menu");
-      }
-    });
-  };
-  toggleMenu();
+  body.addEventListener("click", (e) => {
+    let listMenu = e.target;
+    if (listMenu.closest(".menu") || listMenu.classList.contains("close-btn")) {
+      menu.classList.toggle("active-menu");
+    } else if (!listMenu.matches("menu") && listMenu.tagName != "LI") {
+      menu.classList.remove("active-menu");
+    } else {
+      menu.classList.add("active-menu");
+    }
+  });
 };
 
 export default menu;
