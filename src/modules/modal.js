ac3c71dbe4cb7f22878dca12e9952364c5b171c6
Diana Makarova <din_m-v@mail.ru>
2024-05-28 15:29:08 +0300

    добавлен helpers.js, переписана анимация для модального окна

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -1,3 +1,5 @@
+import { animate } from "./helpers";
+
 const modal = () => {
   const modal = document.querySelector(".popup");
   const buttons = document.querySelectorAll(".popup-btn");
@@ -7,15 +9,16 @@ const modal = () => {
     btn.addEventListener("click", () => {
       if (window.innerWidth > 768) {
         modal.style.display = "block";
-        modalContent.style.opacity = 0;
-        const interval = setInterval(() => {
-          modalContent.style.opacity =
-            parseFloat(modalContent.style.opacity) + 0.1;
-
-          if (modalContent.style.opacity >= 1) {
-            clearInterval(interval);
-          }
-        }, 20);
+        animate({
+          duration: 400,
+          timing(timeFraction) {
+            return timeFraction;
+          },
+          // возвращает определенный отрезок от 0 до 1
+          draw(progress) {
+            modalContent.style.opacity = progress;
+          },
+        });
       } else {
         modal.style.display = "block";
       }
@@ -28,15 +31,19 @@ const modal = () => {
       e.target.classList.contains("popup-close")
     ) {
       if (window.innerWidth > 768) {
-        modalContent.style.opacity = 1;
-        const interval = setInterval(() => {
-          modalContent.style.opacity =
-            parseFloat(modalContent.style.opacity) - 0.1;
-          if (modalContent.style.opacity <= 0) {
-            clearInterval(interval);
-            modal.style.display = "none";
-          }
-        }, 20);
+        animate({
+          duration: 400,
+          timing(timeFraction) {
+            return timeFraction;
+          },
+          // возвращает определенный отрезок от 1 до 0
+          draw(progress) {
+            modalContent.style.opacity = 1 - progress;
+            if (modalContent.style.opacity <= 0) {
+              modal.style.display = "none";
+            }
+          },
+        });
       } else {
         modal.style.display = "none";
       }
