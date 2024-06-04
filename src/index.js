d5cf36c2c7a3bb0ea8cafcede83d02236bc7108c
Diana Makarova <din_m-v@mail.ru>
2024-06-04 16:28:11 +0300

    отправка формы + валидация

--- src/index.js
+++ src/index.js
@@ -3,15 +3,43 @@
 import timer from "./modules/timer";
 import menu from "./modules/menu";
 import modal from "./modules/modal";
-import validator from "./modules/validator";
+// import validator from "./modules/validator";
 import tabs from "./modules/tabs";
 import slider from "./modules/slider";
 import calc from "./modules/calc";
+import sendForm from "./modules/sendForm";
 
 timer("8 july 2024");
 menu();
 modal();
-validator();
+// validator();
 tabs();
 slider();
 calc(100);
+sendForm({
+  formid: "form1",
+  someElem: [
+    {
+      type: "block",
+      id: "total",
+    },
+  ],
+});
+sendForm({
+  formid: "form2",
+  someElem: [
+    {
+      type: "block",
+      id: "total",
+    },
+  ],
+});
+sendForm({
+  formid: "form3",
+  someElem: [
+    {
+      type: "block",
+      id: "total",
+    },
+  ],
+});
